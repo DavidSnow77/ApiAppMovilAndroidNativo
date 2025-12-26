8fcdf57535d80a484ace4c27ace12adcec728a09
DavidSnow77 <20233l001068@utcv.edu.mx>
2025-12-26 17:10:19 -0600

    VISTA_DETALLE_VENTAS

---
+++ Migrations/20251226230854_CrearVistaDetalleVentas.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Abarrotes.Migrations
+{
+    public partial class CrearVistaDetalleVentas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE OR REPLACE VIEW vista_detalle_ventas AS
+                SELECT 
+                    v.""VentaId""        AS ""VentaId"",
+                    v.""FechaVenta""     AS ""FechaVenta"",
+                    p.""NombreProducto"" AS ""Producto"",
+                    p.""Precio""         AS ""Precio"",
+                    d.""Cantidad""       AS ""Cantidad"",
+                    d.""Estado""         AS ""Estado"",
+                    (p.""Precio"" * d.""Cantidad"") AS ""Total""
+                FROM ""DetalleVentas"" d
+                JOIN ""Ventas""     v ON d.""VentaId""    = v.""VentaId""
+                JOIN ""Productos""  p ON d.""ProductoId"" = p.""ProductoId"";
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP VIEW IF EXISTS vista_detalle_ventas;");
+        }
+    }
+}
